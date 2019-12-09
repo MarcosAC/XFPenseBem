3cedb7c55457138bcb44468b83e705aa8432ce54
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-12-09 11:06:26 -0300

    FrontEnd

---
+++ PenseBem/PenseBem/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PenseBem.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace PenseBem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            #if DEBUG
+                HotReloader.Current.Start(this);
+            #endif
+
+            MainPage = new Principal();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
